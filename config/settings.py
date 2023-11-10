08700d29b685b4ca1a6c284cca995a4324f17596
ikromovisroil <ikromovisroil0309@gmail.com>
2023-11-10 20:55:33 +0500

    qwe

--- config/settings.py
+++ config/settings.py
@@ -129,9 +129,8 @@ MEDIA_ROOT = 'file_img'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-    ]
-
+    str(BASE_DIR.joinpath('static'))
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
